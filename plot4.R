13dba606bd5a94c654010b203d9532c6459c3e1a
kaplanae <kaplanae@gmail.com>
2015-09-21 11:49:30 -0500

    files added

--- plot4.R
+++ plot4.R
@@ -1,15 +1,15 @@
 fileUrl <- 'https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip'
 download.file(fileUrl, destfile ="./HouseholdPower")
 unzip("./HouseholdPower")
-HHPower <-read.table(unzip("./HouseholdPower"), header = TRUE, sep = ";")
+HHPower <-read.table(unzip("./HouseholdPower"), na.strings = "?", header = TRUE, sep = ";")
 head(HHPower)
 str(HHPower)
-HHPower <- subset(HHPower, Date == "2/1/2007" | Date == "2/2/2007")
+HHPower <- subset(HHPower, Date == "1/2/2007" | Date == "2/2/2007")
 HHPower[, c(3:9)] <- sapply(HHPower[, c(3:9)], as.character)
 HHPower[, c(3:9)] <- sapply(HHPower[, c(3:9)], as.numeric)
-HHPower$Date <-as.Date(HHPower$Date, format = "%m/%d/%Y")
-HHPower <- transform(HHPower, timestamp=as.POSIXct(paste(Date, Time)), "%m/%d/%Y %H:%M:%S")
-jpeg('rplot4.jpg', width= 480, height = 480)
+HHPower$Date <-as.Date(HHPower$Date, format = "%d/%m/%Y")
+HHPower <- transform(HHPower, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+png('rplot4.png', width= 480, height = 480)
 par(mfrow = c(2,2))
 #plot 1
 plot(HHPower$timestamp, HHPower$Global_active_power, type="l", ylim = c(0,6), xlab="", ylab="Global Active Power")
@@ -25,3 +25,4 @@ legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_meteri
 plot(HHPower$timestamp, HHPower$Global_reactive_power, type = "l",
      ylim = c(0,0.5), xlab = "datetime", ylab = "Global_reactive_power")
 dev.off()
+
